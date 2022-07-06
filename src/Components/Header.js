e16b493c940c9aaf578a69314ad60e0102762229
Sarath1908 <kirlasarath@gmail.com>
2022-07-06 23:40:48 +0530

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return(
+        <div className='header'>
+           <h3>ToDo List</h3> 
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import '../'
+
+
+
+const Home = () => {
+    return (
+        <div className = 'homecontainer'>
+            <Header />
+
+            <Footer/>
+        </div>
+    )
+}
+
+export default Home;
